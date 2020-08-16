3671be1a2b44bdd120a4a52d72dde3124830d6fb
Julien Poissonnier <julien@caffeine.lu>
2020-08-16 09:38:34 +0200

    Add test to assert timeout works

---
+++ __test__/timeout.test.ts
@@ -0,0 +1,35 @@
+import { Server, createServer, IncomingMessage, ServerResponse } from "http";
+import nodeFetch from "node-fetch";
+import { getAddr, listen } from "./util";
+import createFetch from "../src";
+
+const fetch = createFetch(nodeFetch, { timeout: 2000 });
+let servers: Server[] = [];
+
+afterEach(() => {
+	while (servers.length) {
+		const server = servers.pop();
+
+		if (!server) {
+			continue;
+		}
+
+		server.close();
+	}
+});
+
+test("Times out after the specified time", async () => {
+	const server = createServer(
+		(_req: IncomingMessage, _res: ServerResponse) => {
+			// hang
+		}
+	);
+	servers.push(server);
+	await listen(server);
+
+	const { port } = getAddr(server);
+
+	await expect(
+		fetch(`http://127.0.0.1:${port}`, { retry: { retries: 0 } })
+	).rejects.toThrow(/Socket timeout/);
+});
